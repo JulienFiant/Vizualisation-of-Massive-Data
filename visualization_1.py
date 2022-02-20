6220bf6e7f7ed1e53e5e2ba40dca98b91bca444a
Mael Brocher <mael.brocher@epitech.eu>
2022-02-20 14:53:02 +0100

    visualisations

--- visualization_1.py
+++ visualization_1.py
@@ -2,21 +2,23 @@ import numpy as np
 import pandas as pd
 from math import *
 import argparse
+import matplotlib.pyplot as plt
+import seaborn as sb
 
 def isColumnExist(nameColumn, dataHeader):
     return (nameColumn in dataHeader)
 
 def main():
+    data = pd.read_csv("data.csv",delimiter=";")
     parser = argparse.ArgumentParser(description='Visualization 1')
-    parser.add_argument('--range', '-r', type=int, help='range of points', required=False, default=50)
-    parser.add_argument('--column1', '-c1', type=str, help='Name of the first column to visualize', required=False, default="Car")
-    parser.add_argument('--column2', '-c2', type=str, help='Name of the second column to visualize', required=False, default="Origin")
+    parser.add_argument('--range', '-r', type=int, help='range of points', required=False, default=len(data))
+    parser.add_argument('--column1', '-c1', type=str, help='Name of the first column to visualize', required=False, default="Horsepower")
+    parser.add_argument('--column2', '-c2', type=str, help='Name of the second column to visualize', required=False, default="Acceleration")
     args = parser.parse_args()
     print("args.range=%s" % args.range)
     print("args.column1=%s" % args.column1)
     print("args.column2=%s" % args.column2)
 
-    data = pd.read_csv("data.csv",delimiter=";")
     if (isColumnExist(args.column1, data.columns) == False):
         print("Error column : " + args.column1 + " doesn't exist in the dataset")
         return (1)
@@ -26,7 +28,8 @@ def main():
     if (args.range > len(data.index) or args.range < 1):
         print ("Error in the argument range, change the value")
         return (1)
-
+    ax = sb.scatterplot(x=data[args.column2][0:args.range], y=data[args.column1][0:args.range])
+    plt.show()
     return
 
 if __name__ == "__main__":
