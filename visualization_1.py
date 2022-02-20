61ef3e25a7e1335d7decaff617848aeea07e05a9
Fiant <julien.fiant@epitech.eu>
2022-02-20 12:23:56 +0100

    gestion d'erreur dans les argparse visualization

--- visualization_1.py
+++ visualization_1.py
@@ -3,6 +3,9 @@ import pandas as pd
 from math import *
 import argparse
 
+def isColumnExist(nameColumn, dataHeader):
+    return (nameColumn in dataHeader)
+
 def main():
     parser = argparse.ArgumentParser(description='Visualization 1')
     parser.add_argument('--range', '-r', type=int, help='range of points', required=False, default=50)
@@ -12,6 +15,18 @@ def main():
     print("args.range=%s" % args.range)
     print("args.column1=%s" % args.column1)
     print("args.column2=%s" % args.column2)
+
+    data = pd.read_csv("data.csv",delimiter=";")
+    if (isColumnExist(args.column1, data.columns) == False):
+        print("Error column : " + args.column1 + " doesn't exist in the dataset")
+        return (1)
+    if (isColumnExist(args.column2, data.columns) == False):
+        print("Error column : " + args.column2 + " doesn't exist in the dataset")
+        return (1)
+    if (args.range > len(data.index) or args.range < 1):
+        print ("Error in the argument range, change the value")
+        return (1)
+
     return
 
 if __name__ == "__main__":
